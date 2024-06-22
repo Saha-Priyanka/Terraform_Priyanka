6adc83ba206b84757b1261fac1ceedb9e3ba20f7
PRIYANKA SAHA <114468969+Saha-Priyanka@users.noreply.github.com>
2024-06-22 18:06:23 +0530

    Update backend.tf

--- terraform/Infrastructure_Creation/backend.tf
+++ terraform/Infrastructure_Creation/backend.tf
@@ -1,12 +1,12 @@
 terraform {
-    required_version = "0.14.8"
+    # required_version = "0.14.8"
     backend "azurerm" {
         resource_group_name = "Schnieder_hemant"
         storage_account_name = "stogterraformstate01"
         container_name = "tfstate-p"
         key = "terraform.tfstate"
-        arm_subscription_id = "793729a4-a745-4e4f-8b8d-88bda288a86a"
-        arm_tenant_id = "1e9cc706-c3fd-4b8c-9dbd-a073e7384b56"
+       # arm_subscription_id = "793729a4-a745-4e4f-8b8d-88bda288a86a"
+       # arm_tenant_id = "1e9cc706-c3fd-4b8c-9dbd-a073e7384b56"
         
     }
 }
